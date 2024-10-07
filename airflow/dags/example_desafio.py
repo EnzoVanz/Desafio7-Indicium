9f884d63dfe9a4e475b7ba364e061ddc5d18fc2a
Enzo Vanzuita <enzo.piazera@indicium.tech>
2024-10-07 20:10:39 -0300

    1 test dag

---
+++ airflow/dags/example_desafio.py
@@ -0,0 +1,100 @@
+from airflow.utils.edgemodifier import Label
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow import DAG
+from airflow.models import Variable
+import sqlite3
+import pandas as pd
+import pandasql as ps
+
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+## Do not change the code below this line ---------------------!!#
+def export_final_answer():
+    import base64
+
+    # Import count
+    with open('count.txt') as f:
+        count = f.readlines()[0]
+
+    my_email = Variable.get("my_email")
+    message = my_email+count
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+
+    with open("final_output.txt","w") as f:
+        f.write(base64_message)
+    return None
+## Do not change the code above this line-----------------------##
+
+def read_orders_to_csv():
+    conn = sqlite3.connect('../../data/Northwind_small.sqlite')
+    df = pd.read_sql_query("SELECT * FROM 'Order'", conn)
+    df.to_csv('output_orders.csv', index=False)
+    conn.close()
+
+
+def count():
+    conn = sqlite3.connect('../../data/Northwind_small.sqlite')
+    df_orderdetail = pd.read_sql_query("SELECT * FROM 'OrderDetail'", conn)
+    conn.close()
+
+    df_orders = pd.read_csv('output_orders.csv', index=False)
+    df_merged = pd.merge(df_orders, df_orderdetail, left_on='Id', right_on='OrderId', how='inner')
+
+    query = """
+    SELECT SUM(Quantity) 
+    FROM df_merged 
+    WHERE ShipCity = 'Rio de Janeiro';
+    """
+
+    query_result = ps.sqldf(query, locals())
+    count = str(query_result['SUM(Quantity)'].iloc[0])
+    with open('count.txt', 'w') as f:
+        f.write(count)
+
+
+with DAG(
+    'DesafioAirflow',
+    default_args=default_args,
+    description='Desafio de Airflow da Indicium',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2024, 10, 1),
+    catchup=True,
+    tags=['example'],
+) as dag:
+    dag.doc_md = """
+        Esse é o desafio de Airflow da Indicium.
+    """
+   
+    export_final_output = PythonOperator(
+        task_id='export_final_output',
+        python_callable=export_final_answer,
+        provide_context=True
+    )
+
+    read_orders_task = PythonOperator(
+        task_id='read_orders',
+        python_callable=read_orders_to_csv,
+    )
+
+    count_task = PythonOperator(
+        task_id='count_quantity_rio',
+        python_callable=count,
+    )
+
+    read_orders_task >> count_task >> export_final_output
--- dockerfile
+++ dockerfile
@@ -2,8 +2,18 @@ FROM apache/airflow:latest
 
 USER root
 
+# Instalar Git
 RUN apt-get update && \
     apt-get -y install git && \
     apt-get clean
+
+# Copiar o arquivo requirements.txt para o contêiner
+COPY requirements.txt .
+
+# Mudar para o usuário airflow
+USER airflow
+
+# Instalar dependências do Python
+RUN pip install --no-cache-dir -r requirements.txt
 
 USER airflow
